746b440e30170db4b679c431f65ec09e4fcc834e
Filipp Gorbunov <Filippgorb@gmail.com>
2018-11-28 16:13:08 -0800

    remade the landing page and bio

--- src/App.js
+++ src/App.js
@@ -4,21 +4,39 @@ import myPicture from './pictures/MyFace.PNG';
 import weatherFM from './pictures/weatherfm.png';
 import whereShouldWeEat from './pictures/WhereShouldWeEat.PNG';
 import iLearn from './pictures/ilearn.png';
+import Typing from 'react-typing-animation';
+import AnchorLink from 'react-anchor-link-smooth-scroll';
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
 
-        <header className="App-header">
-          <h2 className="App-title">Full Stack Developer</h2>
-          <h1 className="my-name">Filipp Gorbunov</h1>
-          <img className="my-picture" src={myPicture} alt="Me smiling the beach of the Pacific Ocean"/>
-          <p className="bio">
+        {/* <ul className="navbar">
+          <li><a href="#Main">Main</a></li>
+          <li><a href="#Projects">Projects</a></li>
+          <li><a href="#Contact-Info">Contact Info</a></li>
+        </ul> */}
+
+        <header className="App-header" id="Main">
+          {/* <h2 className="App-title">Full Stack Developer</h2> */}
+          <Typing>
+            <h1 className="my-name">Filipp Gorbunov</h1>
+
+            <p>
+              <AnchorLink href="#About-Me">About Me</AnchorLink>
+              <AnchorLink href="#Projects">Projects</AnchorLink>
+              <AnchorLink href="#Contact-Info">Contact Info</AnchorLink>
+            </p>
+          </Typing>
+
+          {/* <img className="my-picture" src={myPicture} alt="Me smiling the beach of the Pacific Ocean"/> */}
+          {/* <p className="bio">
             Hey, my name is <span className="name-span">Filipp</span>. I am a full stack developer located in <span className="state-span">
               <a href="https://www.wta.org/go-outside/map" target="_blank" rel="noopener noreferrer" className="state-link">WASHINGTON</a></span> State. I enjoy creating apps
             using <span className="react-span">React</span>, and I strive to constantly learn new things about coding and development. Check out my <span className="name-span">projects</span> below!
-          </p>
+          </p> */}
           {/* <p className="bio">
             Before getting into programming, I studied Accounting and spent time volunteering for United Way, where I helped low income individuals
             prepare their taxes. After that I worked as a Co-Lead/Software tester for Microsoft, testing the at-the-time unreleased Xbox One X. I have 
@@ -31,7 +49,30 @@ class App extends Component {
           </p> */}
         </header>
 
-        <section className="project-div">
+        <section className="about-me-div" id="About-Me">
+          <h3 className="div-header">About Me</h3>
+          <img className="my-picture" src={myPicture} alt="Me smiling the beach of the Pacific Ocean"/>
+          {/* <p className="bio">
+            Hey, my name is <span className="name-span">Filipp</span>. I used to be a software tester, now turned a full stack developer, located in <span className="state-span">
+              <a href="https://www.wta.org/go-outside/map" target="_blank" rel="noopener noreferrer" className="state-link">WASHINGTON</a></span> State. I enjoy creating apps
+            using <span className="react-span">React</span>, and I strive to constantly learn new things about coding and development. Check out my <span className="name-span">projects</span> below!
+          </p> */}
+          <Typing speed={1}>
+            <Typing.Delay ms={4000} />
+            <ul className="bio">
+              <li>NAME: Filipp Gorbunov</li>
+              <li>BACKGROUND: Software Tester turned Full Stack Developer</li>
+              <li>LOCATION: Beautiful state of <span className="state-span">
+                <a href="https://www.wta.org/go-outside/map" target="_blank" rel="noopener noreferrer" className="state-link">WASHINGTON</a></span></li>
+              <li>ABOUT ME: I enjoy creating apps using <span className="react-span">React,</span></li>
+              <li>I strive to constantly learn new things about coding and development.</li>
+              <li>PROJECTS: Click <span className="name-span"><AnchorLink href="#Projects">HERE</AnchorLink></span> to check them out below!</li>
+            </ul>
+          </Typing>
+          
+        </section>
+
+        <section className="project-div" id="Projects">
           <h3 className="div-header">Projects</h3>
           <div className="project">
             <h4 className="project-name">WeatherFM</h4>
@@ -92,13 +133,14 @@ class App extends Component {
           </div>
         </section>
 
-        <section className="contact-div">
+        <section className="contact-div" id="Contact-Info">
           <h3 className="div-header">Wanna talk? Contact Me!</h3>
           <ul className="contact-icons">
             <li className="icon"><a href="https://www.linkedin.com/in/filippgorbunov/" target="_blank" rel="noopener noreferrer"><img src="https://png.icons8.com/ios/50/000000/linkedin.png" alt="LinkedIn Logo"/></a></li>
             <li className="icon"><a href="https://github.com/Filtos" target="_blank" rel="noopener noreferrer"><img src="https://png.icons8.com/ios/50/000000/github.png" alt="GitHub Logo"/></a></li>
             <li className="icon"><a href="mailto:Filippgorb@gmail.com" target="_blank" rel="noopener noreferrer"><img src="https://png.icons8.com/ios/50/000000/new-post.png" alt="eMail Logo"/></a></li>
           </ul>
+          <AnchorLink href="#Main" className="back-to-top">Back To Top</AnchorLink>
         </section>
 
       </div>
